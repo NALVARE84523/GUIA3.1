bb97d2de39664e9c2e042f972b308567b51eb851
Nelson Alvarez <nalvare84523@universidadean.edu.co>
2020-03-08 20:37:38 -0500

    Agregar

---
+++ Pendulo.java
@@ -0,0 +1,44 @@
+import clases.pen;
+import java.util.Scanner;
+
+
+public class Pendulo {
+
+	public static void main(String[] args) {
+// Clase nombre = new Clase();
+		
+		/*
+		do{
+		
+		
+		
+		}while(CONDICION);
+		
+		while(CONDICION){
+		
+		
+		}
+		
+		
+		*/
+		int flag = 0;
+		do {
+			double l, g = 0;
+			Scanner leer = new Scanner(System.in);
+			System.out.println("Digite la longitud de la cuerda");
+			l = Double.parseDouble(leer.nextLine());
+			
+			System.out.println("Digite la aceleracion gravitacional");
+			g = Double.parseDouble(leer.nextLine());
+			
+			
+			pen pen1 = new pen();
+			pen1.setLongitud(l);
+			pen1.setAceleracionGravitacional(g);
+			pen1.calcularUno();
+			System.out.println("Si desea salir, digite 1, de lo contrario digite 0");
+			flag = Integer.parseInt(leer.nextLine());
+		}while(flag != 0);
+
+}
+}
